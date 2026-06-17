500baaa990671d094ce8860d6efc60394b63edd7
rogozinvmikhail-max <rogozin.v.mikhail@gmail.com>
2026-06-17 19:01:08 +0100

    Update send-invoice.js

--- functions/api/send-invoice.js
+++ functions/api/send-invoice.js
@@ -2,7 +2,7 @@ async function sendEmail(apiKey, { to, subject, html }) {
   return fetch('https://api.resend.com/emails', {
     method: 'POST',
     headers: { 'Authorization': `Bearer ${apiKey}`, 'Content-Type': 'application/json' },
-    body: JSON.stringify({ from: 'ColdCore <invoices@coldcore.uk>', to, subject, html }),
+    body: JSON.stringify({ from: 'ColdCore <sales@coldcore.uk>', to, subject, html }),
   });
 }
 
