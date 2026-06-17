71002e199d34aedc39cc15ecd08abc0043be7a76
rogozinvmikhail-max <rogozin.v.mikhail@gmail.com>
2026-06-17 18:36:51 +0100

    Add files via upload

--- functions/api/stripe-webhook.js
+++ functions/api/stripe-webhook.js
@@ -21,7 +21,7 @@ async function sendEmail(apiKey, { to, subject, html }) {
   return fetch('https://api.resend.com/emails', {
     method: 'POST',
     headers: { 'Authorization': `Bearer ${apiKey}`, 'Content-Type': 'application/json' },
-    body: JSON.stringify({ from: 'ColdCore Orders <orders@coldcore.uk>', to, subject, html }),
+    body: JSON.stringify({ from: 'ColdCore <onboarding@resend.dev>', to, subject, html }),
   });
 }
 
